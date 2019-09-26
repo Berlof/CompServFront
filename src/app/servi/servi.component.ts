ade0d22d33288f63becc5dfc611e599acd581faa
Berlof <nikita73rus@gmail.com>
2019-09-26 19:20:32 +0400

    Cделал лабу 3

---
+++ src/app/servi/servi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servi',
+  templateUrl: './servi.component.html',
+  styleUrls: ['./servi.component.less']
+})
+export class ServiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
